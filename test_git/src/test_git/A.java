50eb9044be1b2b850f5cf2636d1f1f7c4c30062a
CHOIJEONGUK <slalal0022@gmail.com>
2018-10-30 20:37:33 +0900

    aaaaa

--- test_git/src/test_git/A.java
+++ test_git/src/test_git/A.java
@@ -3,6 +3,7 @@ package test_git;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("hello it is git_test");
+		System.out.println("hello it is git_test@@@@@@@@@@@@@@");
 	}
 }
 
